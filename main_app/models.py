30f15c02a0529849d74426bbb110b43be78e2845
Oleh Smolynets <smolynets@gmail.com>
2019-05-04 15:28:09 +0300

    Add Frame model

--- main_app/models.py
+++ main_app/models.py
@@ -2,4 +2,12 @@ from django.db import models
 
 
 class Frame(models.Model):
-    text = models.CharField(max_length=1000)
+	name = models.CharField(max_length=20)
+	text = models.TextField(blank=False)
+
+	class Meta:
+		verbose_name = 'Frame'
+		verbose_name_plural = 'Frames'
+
+	def __str__(self):
+		return self.name
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from . models import Frame
 
 
 def index(request):
-	return render(request, 'index.html')
+	frames = Frame.objects.all()
+	return render(request, 'index.html', {"frames": frames})
