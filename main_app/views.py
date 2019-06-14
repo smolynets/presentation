1317eb3029c696a8a53db02575af5ca16fca7cb1
Oleh Smolynets <smolynets@gmail.com>
2019-06-14 22:07:00 +0300

    fix: fixed header

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from . models import Frame, PageOfSlider, PresentasionName, SubheaderText
+from . models import Frame, PageOfSlider, PresentasionName, SubheaderText, SubheaderImage
 
 
 def index(request):
@@ -7,7 +7,9 @@ def index(request):
 	slides = PageOfSlider.objects.all()
 	presentation_name = PresentasionName.objects.get()
 	subheader_text = SubheaderText.objects.all().first()
+	subheader_image = SubheaderImage.objects.all().first()
 	return render(request, 'index.html', {"frames": frames,
 										  "slides": slides,
 										  "presentation_name": presentation_name,
-										  "subheader_text": subheader_text})
+										  "subheader_text": subheader_text,
+										  "subheader_image": subheader_image})
