7bb1aa56edcfb3642f2b3b52d1416b416a7610f4
Oleh Smolynets <smolynets@gmail.com>
2019-06-14 22:11:00 +0300

    fix: imports

--- main_app/admin.py
+++ main_app/admin.py
@@ -2,8 +2,7 @@ from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
 from solo.admin import SingletonModelAdmin
 
-from .models import Frame, PageOfSlider, PresentasionName, SubheaderText, SubheaderImage
-
+from .models import Frame, PageOfSlider, PresentasionName, SubheaderText
 
 class Sumernote(SummernoteModelAdmin):
 	summernote_fields = ('text',)
@@ -12,4 +11,3 @@ admin.site.register(Frame, Sumernote)
 admin.site.register(PageOfSlider, Sumernote)
 admin.site.register(SubheaderText)
 admin.site.register(PresentasionName, SingletonModelAdmin)
-admin.site.register(SubheaderImage)
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from . models import Frame, PageOfSlider, PresentasionName, SubheaderText, SubheaderImage
+from . models import Frame, PageOfSlider, PresentasionName, SubheaderText
 
 
 def index(request):
@@ -7,9 +7,7 @@ def index(request):
 	slides = PageOfSlider.objects.all()
 	presentation_name = PresentasionName.objects.get()
 	subheader_text = SubheaderText.objects.all().first()
-	subheader_image = SubheaderImage.objects.all().first()
 	return render(request, 'index.html', {"frames": frames,
 										  "slides": slides,
 										  "presentation_name": presentation_name,
-										  "subheader_text": subheader_text,
-										  "subheader_image": subheader_image})
+										  "subheader_text": subheader_text})
