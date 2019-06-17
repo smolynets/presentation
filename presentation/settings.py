4fc09618efeaa0977c09d0ef8e8e84573ce104c2
Oleh Smolynets <smolynets@gmail.com>
2019-06-17 13:04:30 +0300

    fix: changed settings

--- presentation/settings.py
+++ presentation/settings.py
@@ -115,10 +115,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-try:
-    STATIC_URL = '/static/'
-except:
-    STATIC_ROOT
+STATIC_URL = '/static/'
+
+STATIC_ROOT
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
