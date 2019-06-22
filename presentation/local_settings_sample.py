1f3918e6c79e011c348de701242aa069ca91793a
Oleh Smolynets <smolynets@gmail.com>
2019-06-22 14:01:37 +0300

    fix: local_settings_sample.py

--- presentation/local_settings_sample.py
+++ presentation/local_settings_sample.py
@@ -23,4 +23,4 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 #     }
 }
 
-STATIC_ROOT = os.path.join(BASE_DIR, ".." "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "..", "static")
